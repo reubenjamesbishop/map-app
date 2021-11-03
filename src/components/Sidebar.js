7fb64b699d15e4865524e9071a258a38c40083dc
reubenjamesbishop <reubenjamesbishop@gmail.com>
2021-11-03 23:05:40 +1030

    Added sidebar and fixed styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,18 +7,30 @@ import './Sidebar.css';
 
 export default (props) => {
   return (
-    <Menu disableAutoFocus>
+    <Menu disableAutoFocus isOpen={true}>
       <a id="home" className="menu-item" href="/">
-        <i class="bi bi-activity"></i> Home
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="0.8em"
+          height="0.8em"
+          fill="currentColor"
+          class="bi bi-brightness-high"
+          viewBox="0 0 16 16"
+          class="sidebar-icon"
+        >
+          <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+        </svg>
+        Home
       </a>
       <a id="about" className="menu-item" href="/about">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
+          width="0.8em"
+          height="0.8em"
           fill="currentColor"
           class="bi bi-activity"
           viewBox="0 0 16 16"
+          class="sidebar-icon"
         >
           <path
             fill-rule="evenodd"
@@ -28,11 +40,38 @@ export default (props) => {
         About
       </a>
       <a id="contact" className="menu-item" href="/contact">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="0.8em"
+          height="0.8em"
+          fill="currentColor"
+          class="bi bi-basket"
+          viewBox="0 0 16 16"
+          class="sidebar-icon"
+        >
+          <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z" />
+        </svg>
         Contact
       </a>
-      <a onClick={this.showSettings} className="menu-item--small" href="">
+      <a onClick={this.showSettings} className="menu-item" href="">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="0.8em"
+          height="0.8em"
+          fill="currentColor"
+          class="bi bi-bookmark-dash"
+          viewBox="0 0 16 16"
+          class="sidebar-icon"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.5 6.5A.5.5 0 0 1 6 6h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z"
+          />
+          <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />
+        </svg>
         Settings
       </a>
+      <a id="author-tag">Reuben Bishop, 2021.</a>
     </Menu>
   );
 };
