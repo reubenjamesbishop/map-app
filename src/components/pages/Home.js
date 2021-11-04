5cbaccabccce3552b061a06e56c24a4eeea00702
reubenjamesbishop <reubenjamesbishop@gmail.com>
2021-11-04 16:20:51 +1030

    1 file modified

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 
-// https://api.openweathermap.org/data/2.5/weather?q={Sydney}&appid={apikey}
+// function getResult(city) {
+//   fetch(
+//     'https://api.openweathermap.org/data/2.5/weather?q=' +
+//       city +
+//       '&appid=d4da841c84606053b9db5e5a9b5b800f'
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log('Data is ', data);
+//       console.log(
+//         'Weather for ' + data.name + ' is: ' + data.weather[0].description
+//       );
+//     });
+// }
 
-// https://api.openweathermap.org/data/2.5/weather?q=Adelaide&appid=d4da841c84606053b9db5e5a9b5b800f
+function Home(props) {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
 
-function getResult(city) {
-  fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q=' +
-      city +
-      '&appid=d4da841c84606053b9db5e5a9b5b800f'
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('Data is ', data);
-      console.log(
-        'Weather for ' + data.name + ' is: ' + data.weather[0].description
-      );
-    });
-}
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
 
-function Home() {
-  getResult('Adelaide');
   return (
     <div class="home-wrapper">
       <h1 class="home-title">Weather</h1>
-      <p>globalResponse is...</p>
+      <p>Count: {count}</p>
+      <button class="btn btn-success" onClick={() => setCount(count + 1)}>
+        Counter
+      </button>
     </div>
   );
 }
