ec6701db9db2bf1d7bbaed905ae73720d4f0080d
reubenjamesbishop <reubenjamesbishop@gmail.com>
2021-11-04 17:08:40 +1030

    2 files modified

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,38 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import './Home.css';
 
-// function getResult(city) {
-//   fetch(
-//     'https://api.openweathermap.org/data/2.5/weather?q=' +
-//       city +
-//       '&appid=d4da841c84606053b9db5e5a9b5b800f'
-//   )
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log('Data is ', data);
-//       console.log(
-//         'Weather for ' + data.name + ' is: ' + data.weather[0].description
-//       );
-//     });
-// }
-
 function Home(props) {
   // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+  // const [weatherdata, set_weatherdata] = useState('No Data');
+  const [cityName, set_cityName] = useState('No cityName');
+  const [weatherDescription, set_weatherDescription] = useState(
+    'No weatherDescription'
+  );
 
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
+    // send HTTP request then save response to variable
+    fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=Adelaide&appid=d4da841c84606053b9db5e5a9b5b800f'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // set_weatherdata(data.name);
+        set_cityName(data.name);
+        set_weatherDescription(data.weather[0].description);
+      });
   });
 
   return (
     <div class="home-wrapper">
-      <h1 class="home-title">Weather</h1>
-      <p>Count: {count}</p>
-      <button class="btn btn-success" onClick={() => setCount(count + 1)}>
-        Counter
-      </button>
+      <h1 class="home-title">🌞 wearther.io 🌞</h1>
+      <p>City: {cityName}</p>
+      <p>Weather description: {weatherDescription}</p>
     </div>
   );
 }
