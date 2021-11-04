7b968ec8f6b6317024f627833aa5c6d2701e16cd
reubenjamesbishop <reubenjamesbishop@gmail.com>
2021-11-04 16:08:38 +1030

    1 file modified

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import './Home.css';
 
-// Weather API
 // https://api.openweathermap.org/data/2.5/weather?q={Sydney}&appid={apikey}
 
+// https://api.openweathermap.org/data/2.5/weather?q=Adelaide&appid=d4da841c84606053b9db5e5a9b5b800f
+
+function getResult(city) {
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+      city +
+      '&appid=d4da841c84606053b9db5e5a9b5b800f'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Data is ', data);
+      console.log(
+        'Weather for ' + data.name + ' is: ' + data.weather[0].description
+      );
+    });
+}
+
 function Home() {
+  getResult('Adelaide');
   return (
     <div class="home-wrapper">
-      <h1 class="home-title">Home JS Content</h1>
-      {/* <Viewer /> */}
+      <h1 class="home-title">Weather</h1>
+      <p>globalResponse is...</p>
     </div>
   );
 }
