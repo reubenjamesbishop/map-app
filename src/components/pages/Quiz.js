9e32961fa9ce6acccca575e41567cb437816c0ba
reubenjamesbishop <reubenjamesbishop@gmail.com>
2021-11-09 12:42:25 +1030

    3 files modified

--- src/components/pages/Quiz.js
+++ src/components/pages/Quiz.js
@@ -6,9 +6,21 @@ import QuizCard from '../QuizCard';
 function Quiz(props) {
   // State Variables up Here
   const [count, set_count] = useState(0);
+  const [current_question, set_current_question] = useState(1);
 
   const handleClick = (event) => {
     event.preventDefault();
+    var elmntToView = document.getElementById(
+      // 'question' + current_question.toString()
+      'question3'
+    );
+    elmntToView.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
+    // set_current_question(current_question + 1);
+    // console.log(current_question);
   };
 
   useEffect(() => {
@@ -17,11 +29,11 @@ function Quiz(props) {
   });
 
   return (
-    <>
-      <h1>What beer are you?</h1>
+    <div class="page-wrapper">
+      <h1 onClick={handleClick}>What beer are you?</h1>
       <div class="container">
-        <div class="row">
-          <h3>What would you rather have for breakfast?</h3>
+        <div id="question1" class="row quiz-row">
+          <h3>Question 1: What would you rather have for breakfast?</h3>
           <div class="options">
             <QuizCard
               // onClick={set_count(count + 1)}
@@ -45,8 +57,29 @@ function Quiz(props) {
             />
           </div>
         </div>
-        <div class="row">
-          <h3>What would you rather have for breakfast?</h3>
+        <div id="question1" class="row quiz-row">
+          <h3>Question 2: What would you rather have for breakfast?</h3>
+          <div class="options">
+            <QuizCard
+              name="Croissants"
+              src="https://static01.nyt.com/images/2021/04/07/dining/06croissantsrex1/merlin_184841898_ccc8fb62-ee41-44e8-9ddf-b95b198b88db-articleLarge.jpg"
+            />
+            <QuizCard
+              name="Cereal"
+              src="https://www.verywellhealth.com/thmb/LiEYTC9ZucAPtgIkxivHFQQpXUY=/1333x1000/smart/filters:no_upscale()/30D7A016-ABA5-48DD-BE39-3E7A223A03BF-96f2ba9e6c724dc9b2ba638b0c0f44a2.jpeg"
+            />
+            <QuizCard
+              name="Smoothie"
+              src="https://www.asweetpeachef.com/wp-content/uploads/2017/05/high-protein-fruit-smoothie-recipes-square.jpg"
+            />
+            <QuizCard
+              name="Bacon &amp; Eggs"
+              src="https://i.dietdoctor.com/wp-content/uploads/2015/12/DD-14.jpg?auto=compress%2Cformat&w=1600&h=1067&fit=crop"
+            />
+          </div>
+        </div>
+        <div id="question3" class="row quiz-row">
+          <h3>Question 3: What would you rather have for breakfast?</h3>
           <div class="options">
             <QuizCard
               name="Croissants"
@@ -67,7 +100,7 @@ function Quiz(props) {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
